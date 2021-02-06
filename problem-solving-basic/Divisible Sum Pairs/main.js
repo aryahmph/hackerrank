1a19b8cfe4c64b60055b0de5e26fccc305e6e660
Arya Yunanta <77351340+aryahmph@users.noreply.github.com>
2021-02-06 21:08:09 +0700

    Add files via upload

---
+++ problem-solving-basic/Divisible Sum Pairs/main.js
@@ -0,0 +1,9 @@
+function divisibleSumPairs(n, k, ar) {
+    let result = 0;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if ((ar[i] + ar[j]) % k == 0) result++;
+        }
+    }
+    return result;
+}
